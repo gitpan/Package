47e0e89d26c6915ce1543af40cd849795cd4bf74
Ingy döt Net <ingy@cpan.org>
2011-05-15 15:04:48 +0000

    Import of INGY/Package-0.01 from CPAN.
    gitpan-cpan-distribution: Package
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         INGY/Package-0.01.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

---
+++ lib/Package.pm
@@ -0,0 +1,25 @@
+##
+# name:      Package
+# abstract:  Acmeist Module Packaging Toolchain
+# author:    Ingy döt Net <ingy@cpan.org>
+# copyright: 2011
+# license:   perl
+
+package Package;
+use 5.008003;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 DESCRIPTION
+
+Package is a toolchain for packaging module distributions. Not just Perl
+modules, but modules from many different languages.
+
+=head1 STATUS
+
+This is a super early, proving concepts, release.
+
+The main thing this module does at this point is offer a Module::Install
+plugin called L<Module::Install::Package>.
