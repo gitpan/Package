1abdfea011348362a5677862551c772edd54ce1b
Ingy döt Net <ingy@cpan.org>
2011-10-22 01:34:07 +0000

    Import of INGY/Package-0.15 from CPAN.
    gitpan-cpan-distribution: Package
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         INGY/Package-0.15.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/Package.pm
+++ lib/Package.pm
@@ -17,7 +17,7 @@ use YAML::XS 0.35 ();
 #------------------------------------------------------------------------------#
 package Package;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 #------------------------------------------------------------------------------#
 package Package::Command;
@@ -60,31 +60,18 @@ use Template::Toolkit::Simple;
 use constant abstract => 'Create new module package directory from template';
 use constant usage_desc => 'pkg init --from=<dir> --module=<Name> --to=<dir>';
 
-has from => (
+has dryrun => (
     is => 'ro',
-    isa => 'Str',
-    required => 1,
-    documentation => 'Pkg template directory',
+    isa => 'Bool',
+    default => 0,
+    documentation => 'Create new package but skip git',
 );
 
-has module => (
-    is => 'ro',
-    isa => 'Str',
-    isa => 'ArrayRef[Str]',
-    documentation => 'Name of new module',
-);
-
-has tagline => (
-    is => 'ro',
-    isa => 'Str',
-    default => sub { '' },
-    required => 0,
-    documentation => 'Tagline for package',
-);
 
 sub execute {
     my ($self, $opt, $args) = @_;
-    my $pkg_name = shift(@$args) || '';
+    my $pkg_name = pop(@$args) || '';
+    $self->_args($args);
     
     my $to = io($pkg_name || '.')->absolute;
 
@@ -96,64 +83,114 @@ sub execute {
 
     my $stash = $self->conf->stash;
     $stash->{pkg}{name} = $pkg_name;
-    $stash->{tagline} = $self->tagline;
+    $stash->{author}{name} = $Package::Conf::author_name_hack;
 
     if ($to->exists) {
-        die "$to is not empty" if not $to->empty;
+        die <<"..." if not $to->empty;
+$to is not an empty directory.
+Can't make a new pkg here.
+
+Either specify the name of a new directory you wish to create,
+or name an empty directory, or cd to an empty directory.
+...
     }
     else {
         $to->assert->mkdir;
     }
     $to->chdir or die "Can't chdir to $to";
-    $stash->{module}{name} = $self->module->[0];
 
-    my @special;
     for my $file (sort keys %{$self->conf->manifest}) {
-        if ($file =~ /%/) {
-            push @special, $file;
+        if (not $file =~ /%(.*?)%/) {
+            $self->write_file($file, $file);
             next;
         }
-        my $path = $self->conf->manifest->{$file};
-        my $template = io($path)->all;
-        my $text = tt
-            ->path([])
-            ->data($stash)
-            ->render(\$template);
-        io($file)->assert->print($text);
-    }
-
-    for my $module (@{$self->module}) {
-        $stash->{module}{name} = $module;
-        ($stash->{module}{path} = $module) =~ s!::!/!g;
-        for my $file (@special) {
-            my $path = $self->conf->manifest->{$file};
-            my $template = io($path)->all;
-            (my $local = $file) =~ s!%module\.path%!$stash->{module}{path}!;
-            my $text = tt
-                ->path([])
-                ->data($stash)
-                ->render(\$template);
-            io($local)->assert->print($text);
-        }
+        my $key = $1;
+        my $path = $self->conf->lookup($key)
+            or die "Config variable '$key' is not defined.\n";
+        (my $f = $file) =~ s/%.*?%/$path/;
+        $self->write_file($f, $file);
     }
 
     if ($stash->{git}{create}) {
+        if ($self->dryrun or $to =~ /^.*\/[xyz]{1,5}\d?$/) {
+            (my $url = $stash->{git}{origin}) =~
+                s/\%pkg\.name\%/$stash->{pkg}{name}/e;
+            print <<"...";
+New package '$to' successfully created, but GitHub repo creation skipped.
+
+The GitHub info would be:
+
+    origin: $url
+    repo:   $pkg_name
+    desc:   $stash->{desc}
+...
+            return;
+        }
         system("git init; git add .; git commit -m 'First commit'");
         if (my $url = $stash->{git}{origin}) {
             $url =~ s/\%pkg\.name\%/$stash->{pkg}{name}/e;
             system("git remote add origin $url");
         }
-        # XXX Check Net::Ping->new->ping("github.com");
-        if (my $github = $stash->{git}{github}) {
-            my $login = $github->{login};
-            my $token = $github->{token};
-            my $tagline = $stash->{tagline};
-            system(qq{curl -F login=$login -F token=$token https://github.com/api/v2/yaml/repos/create -F name=$pkg_name -F "description=$tagline"});
-            system("git push origin master");
-        }
+        $self->create_git_repo($pkg_name, $stash->{desc});
     }
 
     print "New package '$to' successfully created!\n";
+}
+
+sub write_file {
+    my ($self, $file, $key) = @_;
+    my $path = $self->conf->manifest->{$key};
+    my $template = io($path)->all;
+    my $text = eval {
+        tt
+        ->path([])
+        ->strict(1)
+        ->data($self->conf->stash)
+        ->render(\$template);
+    };
+    if ($@) {
+        if ($@ =~ /var\.undef error - undefined variable: (\S+)/) {
+            die "Config variable '$1' is not defined.\n";
+        }
+        die $@;
+    }
+    io($file)->assert->print($text);
+    if (-x $path) {
+        chmod 0755, $file;
+    }
+}
+
+#------------------------------------------------------------------------------#
+package Package::Command::repo;
+Package->import( -command );
+use Mouse;
+extends 'Package::Command';
+
+use constant abstract => 'Create a new repo on github';
+use constant usage_desc => 'pkg repo --from=<dir>';
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+    die "Directory has no git repo"
+        unless -d '.git';
+
+    my $stash = $self->conf->stash;
+    for (@$args) {
+        if (/^--(\w+)=(.*)/) {
+            my ($k, $v) = ($1, $2);
+            # $v =~ s/^'(.*)'$/$1/;
+            # $v =~ s/^"(.*)"$/$1/;
+            $stash->{$k} = $v;
+        }
+    }
+
+    my $pkg_name = do {
+        my $cwd = Cwd::cwd;
+        $cwd =~ s!.*/!!;
+        $cwd;
+    };
+
+    $self->create_git_repo($pkg_name, $stash->{desc});
 }
 
 #------------------------------------------------------------------------------#
@@ -163,21 +200,22 @@ use Mouse;
 extends 'Package::Command';
 
 use constant abstract => 'Print list of config variables from template';
-use constant usage_desc => 'pkg liastvars --from=<dir>';
-
-has from => (
-    is => 'ro',
-    isa => 'Str',
-    documentation => 'Pkg template directory',
-);
+use constant usage_desc => 'pkg listvars --from=<dir>';
 
 sub execute {
     my ($self, $opt, $args) = @_;
-    print YAML::XS::Dump($self->get_stash);
+    print YAML::XS::Dump($self->conf->stash);
 }
 
 #------------------------------------------------------------------------------#
 package Package::Command;
+
+has from => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1,
+    documentation => 'Pkg template directory',
+);
 
 has _conf => (
     is => 'ro',
@@ -188,9 +226,27 @@ has _conf => (
         my ($self) = @_;
         Package::Conf->new(
             src_dir => $self->from,
+            cli_args => $self->_args,
         );
     },
 );
+
+has _args => (
+    is => 'rw',
+    default => sub {[]},
+);
+
+sub create_git_repo {
+    my ($self, $pkg_name, $desc) = @_;
+    my $stash = $self->conf->stash;
+    # XXX Check Net::Ping->new->ping("github.com");
+    if (my $github = $stash->{git}{github}) {
+        my $login = $github->{login};
+        my $token = $github->{token};
+        system(qq{curl -F login=$login -F token=$token https://github.com/api/v2/yaml/repos/create -F name=$pkg_name -F "description=$desc"});
+        system("git push origin master");
+    }
+}
 
 1;
 
--- lib/Package/Conf.pm
+++ lib/Package/Conf.pm
@@ -9,15 +9,37 @@ use Mouse;
 
 use Cwd;
 use File::Spec;
+use Hash::Merge;
 use IO::All;
 use YAML::XS;
 
-has src_dir => (is => 'ro');
-has dir_stack => (is => 'rw');
-has stash => (is => 'rw');
-has manifest => (is => 'rw');
+our $author_name_hack;
 
-sub BUILD {
+has src_dir => (
+    is => 'ro',
+    required => 1,
+);
+has cli_args => (
+    is => 'ro',
+    required => 1,
+);
+has dirs => (
+    is => 'ro',
+    builder => 'dirs_builder',
+    lazy => 1,
+);
+has stash => (
+    is => 'ro',
+    builder => 'stash_builder',
+    lazy => 1,
+);
+has manifest => (
+    is => 'ro',
+    builder => 'manifest_builder',
+    lazy => 1,
+);
+
+sub dirs_builder {
     my ($self) = @_;
     my $home = Cwd::cwd;
     my $dir = $self->src_dir;
@@ -36,16 +58,160 @@ sub BUILD {
         pop @dir;
         $dir = File::Spec->catdir(@dir) or die;
     }
+    chdir $home or die;
+    return \@dirs;
+}
 
-    my $stash = {
-        date => {
-            year => (localtime)[5] + 1900,
-            time => do { $_ = `date`; chomp; $_ },
-        },
-    };
+sub manifest_builder {
+    my ($self) = @_;
     my $manifest = {};
-    for (my $i = 0; $i < @dirs; $i++) {
-        my $dir = $dirs[$i];
+    $self->tree_walker(manifest => sub {
+        my ($name, $path) = @_;
+        $manifest->{$name} = Cwd::abs_path($path);
+    });
+    return $manifest;
+}
+
+sub stash_builder {
+    my ($self) = @_;
+    my $stash = {};
+    $self->tree_walker(stash => sub {
+        my $hash = YAML::XS::LoadFile('pkg.conf') || {};
+        $author_name_hack ||= $hash->{author}{name};
+        $stash = Hash::Merge::merge($hash, $stash);
+    });
+    $stash->{date}{year} = (localtime)[5] + 1900;
+    $stash->{date}{time} = do { $_ = `date`; chomp; $_ };
+    $stash = Hash::Merge::merge(
+        $self->cli_args_hash, $stash,
+    );
+    my @keys = keys %$stash;
+    for my $k (@keys) {
+        next unless $k =~ /\./;
+        $stash = Hash::Merge::merge(
+            $self->hashlet($k, delete $stash->{$k}),
+            $stash,
+        );
+    }
+
+    $self->{stash} = $stash;
+    if (my $rules = delete $stash->{pkg}{rules}) {
+        for my $rule (@$rules) {
+            $self->apply($rule);
+        }
+    }
+    $stash = $self->{stash};
+
+    return $stash;
+}
+
+sub apply {
+    my ($self, $rule) = @_;
+    $rule;
+    my $method = $self->get_method($rule);
+    die "$method rule not supported"
+        unless $self->can($method);
+    return $self->$method($rule);
+}
+
+sub get_method {
+    my ($self, $args) = @_;
+    return "apply_" . shift @$args;
+}
+
+sub get_arg {
+    my ($self, $args) = @_;
+    my $arg = shift @$args;
+    return ref($arg)
+    ? $self->apply($arg)
+    : $arg;
+}
+
+sub set_value {
+    my ($self, $key, $value) = @_;
+    my $hash = $self->hashlet($key, $value);
+    my $stash = $self->{stash};
+    delete $stash->{$key};
+    $self->{stash} = Hash::Merge::merge(
+        $hash,
+        $stash,
+    );
+    return $value;
+}
+
+sub apply_get {
+    my ($self, $args) = @_;
+    my $key = $self->get_arg($args);
+    return $self->lookup($key);
+}
+
+sub apply_init {
+    my ($self, $args) = @_;
+    my $name = $self->get_arg($args);
+    my $value = $self->lookup($name);
+    return $value if defined $value;
+    $value = $self->get_arg($args);
+    return unless defined $value;
+    return $self->set_value($name, $value);
+}
+
+sub apply_replace {
+    my ($self, $args) = @_;
+    my $val = $self->get_arg($args);
+    my $pat = $self->get_arg($args);
+    my $rep = $self->get_arg($args);
+    return unless defined $val;
+    $val =~ s/$pat/$rep/g;
+    return $val;
+}
+
+sub lookup {
+    my ($self, $k, $v) = @_;
+    $v ||= $self->{stash};
+    while ($k =~ s/(.*?)\.//) {
+        $v = $v->{$1};
+    }
+    return unless defined $v and ref($v) eq 'HASH';
+    return $v->{$k};
+}
+
+sub hashlet {
+    my ($self, $k, $v) = @_;
+    my $h = {};
+    my $p = $h;
+    while ($k =~ s/(.*?)\.//) {
+        $p = $p->{$1} = {};
+    }
+    $p->{$k} = $v;
+    return $h;
+}
+
+sub cli_args_hash {
+    my ($self) = @_;
+    my $hash = {};
+    my $args = $self->cli_args;
+    for my $arg (@$args) {
+        $arg =~ /^--([\w\.]+)(?:=(.*))?$/ or next;
+        my ($k, $v) = ($1, $2);
+        $v = 1 if not defined $v;
+        if (exists $hash->{$k}) {
+            $hash->{$k} = [$hash->{$k}]
+                unless ref $hash->{$k} eq 'ARRAY';
+            push @{$hash->{$k}}, $2;
+        }
+        else {
+            $hash->{$1} = $2;
+        }
+    }
+    return $hash;
+}
+
+sub tree_walker {
+    my ($self, $type, $callback) = @_;
+    my $home = Cwd::cwd;
+    my $dirs = $self->dirs;
+    for (my $i = 0; $i < @$dirs; $i++) {
+        my $dir = $dirs->[$i];
         chdir $dir;
         File::Find::find(sub {
             if (-f 'pkg.conf' and $File::Find::dir ne $File::Find::topdir) {
@@ -53,9 +219,9 @@ sub BUILD {
                 return;
             }
             if ($i == 0) {
-                $stash = Hash::Merge::merge(
-                    $stash, YAML::XS::LoadFile('pkg.conf'),
-                );
+                if ($type eq 'stash') {
+                    $callback->();
+                }
                 $File::Find::prune = 1;
                 return;
             }
@@ -64,18 +230,19 @@ sub BUILD {
                 return;
             }
             if ($_ eq 'pkg.conf') {
-                $stash = Hash::Merge::merge(
-                    $stash, YAML::XS::LoadFile('pkg.conf'),
-                );
+                if ($type eq 'stash') {
+                    $callback->();
+                }
                 return;
             }
+            return if /^\./;
             return if -d;
-            $manifest->{$File::Find::name} = Cwd::abs_path($_);
+            if ($type eq 'manifest') {
+                $callback->($File::Find::name, $_);
+            }
         }, '.');
     }
-    $self->manifest($manifest);
-    $self->stash($stash);
-    chdir $home or die;
+    chdir $home;
 }
 
 1;
